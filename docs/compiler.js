a4abac899435821edfe2c9816dc96e4037ddbd48
Ronak Shah <ronakshahultimate@gmail.com>
2017-04-03 09:27:27 -0700

    Update compiler.js

--- docs/compiler.js
+++ docs/compiler.js
@@ -3,7 +3,7 @@
 var head = document.getElementsByTagName('head')[0];
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = "http://code.jquery.com/jquery-2.2.1.min.js";
+script.src = "https://code.jquery.com/jquery-2.2.1.min.js";
 
 // Then bind the event to the callback function.
 // There are several events for cross browser compatibility.
